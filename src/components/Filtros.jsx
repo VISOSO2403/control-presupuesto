9ed5b5f615e038b1df70fedadd750ad645d97fbd
Oliver Visoso <ovisoso@ucol.com>
2022-02-18 17:59:56 -0600

    Segundo commit

---
+++ src/components/Filtros.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+const Filtros = ({filtro, setFiltro}) => {
+  return (
+    <div className="filtros sombra contenedor">
+      <form action="">
+        <div className="campo">
+          <label htmlFor="">Filtrar Gastos</label>
+          <select value={filtro} onChange={e => setFiltro(e.target.value)}>
+            <option value="">-- Todas las categorias --</option>
+            <option value="ahorro">Ahorro</option>
+            <option value="comida">Comida</option>
+            <option value="gastos">Gatos Varios</option>
+            <option value="ocio">Ocio</option>
+            <option value="salud">Salud</option>
+            <option value="suscripciones">Suscripciones</option>
+          </select>
+        </div>
+      </form>
+    </div>
+  )
+};
+
+export default Filtros;
